a7aeb123cc417e049b51aea11484e60ff9ef3ded
matsgemmeke <matsgemmeke@gmail.com>
2019-01-29 16:43:46 +0100

    init

---
+++ src/main/java/domain/Movie.java
@@ -0,0 +1,23 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class Movie {
+
+    private String title;
+    private ArrayList<MovieScreening> screenings;
+
+    public Movie(String title) {
+        this.title = title;
+
+        screenings = new ArrayList<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void addScreening(MovieScreening screening) {
+        screenings.add(screening);
+    }
+}
